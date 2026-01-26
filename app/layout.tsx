d8df3705c8e1d4573121defc081ef0d71779e726
wellingtonsantana <wellington_163@hotmail.com>
2026-01-26 19:11:43 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
-  title: 'CRM1',
-  description: 'Plataforma genérica de CRM1 para experimentos e validações.'
+  title: 'CRM11',
+  description: 'Plataforma genérica de CRM11 para experimentos e validações.'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
