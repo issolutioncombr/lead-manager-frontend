0b5f7dcfeb4a6bf20f13a279a85eab7bc965fdf3
wellingtonsantana <wellington_163@hotmail.com>
2026-01-29 15:19:58 -0300

    1

--- app/(protected)/integrations/page.tsx
+++ app/(protected)/integrations/page.tsx
@@ -19,9 +19,7 @@ const normalizeScopes = (value?: string) =>
   'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email';
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '';
-const fallbackRedirectUri =
-  process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI ??
-  'https://systema-backup-backend-leads.ogy936.easypanel.host/api/google/oauth/callback';
+const fallbackRedirectUri = process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI ?? '';
 
 type GoogleOAuthStateResponse = {
   state: string;
